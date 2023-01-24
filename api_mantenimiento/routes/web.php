64d702957de663155b3abe263f95f6d0c71d7879
Alba Palacios Perez <albapalaciosp@gmail.com>
2023-01-24 19:53:49 +0100

    Crear rutas

--- api_mantenimiento/routes/web.php
+++ api_mantenimiento/routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'incidencias'], function() {
+    Route::get('/', 'IncidenciaController@mostrarIncidencias')->name('incidencias.mostrar');
+    Route::post('/registrar', 'IncidenciaController@registrarIncidencia')->name('incidencias.registrar');
+    Route::post('/asignarIncidencia/{id_empleado}', 'IncidenciaController@asignarIncidencia')->name('incidencias.asignarIncidencia');
+
+});
+
+Route::group(['prefix' => 'empleados'], function() {
+    Route::post('/altaEmpleado', 'EmpleadoController@registrarEmpleado')->name('empleados.registrar');
+    Route::delete('/bajaEmpleado/{id_empleado}', 'EmpleadoController@eliminarEmpleado')->name('empleados.eliminar');
+    Route::put('/cambioTipoEmpleado/{id_empleado}', 'EmpleadoController@cambioTipoEmpleado')->name('empleados.cambio');
+    
+});
+
+Route::group(['prefix' => 'cuotas'], function() {
+    Route::post('/añadirCuota/{id_cliente}', 'CuotaController@añadirCuota')->name('cuotas.añadir');
+    Route::delete('/borrarCuota/{id_cuota}', 'CuotaController@borrarCuota')->name('cuotas.borrar');
+    Route::get('/listadoCuotas/{id_cliente}', 'CuotaController@listadoCuotas')->name('cuotas.listado');
+    Route::put('/modificarCuota/{id_cuota}', 'CuotaController@modificarCuota')->name('cuotas.modificar');
+});
